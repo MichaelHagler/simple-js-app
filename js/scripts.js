2ccbfe9d390e63ae7a0ab69c18e5580a30c76c3d
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2022-09-26 07:07:11 -0700

    for loop
    part 1 of task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-let pokemonList = [{name: "Squirtle", type: "water", height: 1},
-  {name: "Charmander", type: "fire", height: 1},
-  {name: "Bulbasaur", type: ["grass", "poison"], height: 1}];
+let pokemonList = [
+  {name: "Squirtle", type: "water", height: 0.5},
+  {name: "Charmander", type: "fire", height: 0.6},
+  {name: "Bulbasaur", type: ["grass", "poison"], height: 0.7}
+];
+
+// create a for loop that iterates over each item. 
+// document.write() pokemons name and height (bulbasuar (height: 7))
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+}
