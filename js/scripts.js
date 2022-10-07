c5c5d35ffd9537712e476a3473b5aba8d7b61fbc
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2022-10-06 22:38:26 -0700

    promise poly fill

--- js/scripts.js
+++ js/scripts.js
@@ -35,23 +35,32 @@ const pokemonRepository = (function(){
     return repository;
   }
 
+  //show more details of pokemon
+  function showDetails(pokemon){
+    console.log(pokemon.name);
+  }
+
   // add list of pokemon on webpage
+  /*In your addListItem() function, add an event listener to the button you created.
+   It should listen to a click. As for its event handler function,
+    call the showDetails function there, passing the pokemon object as a parameter when a Pokémon is clicked. 
+    This parameter should be the same parameter as addListItem(). Note: You might have thought of this already,
+     but you don’t need to select the button using querySelector() to add event listeners if you’ve already created the element using createElement() within your JavaScript. 
+     Simply add the event listener to the variable that’s been assigned with document.createElement('button')*/
+
   function addListItem(pokemon){
     const pokemonList = document.querySelector(".pokemon-list");
     const listpokemon = document.createElement('li');
     const button = document.createElement('button');
 
-    button.addEventListener('click', showDetails);
     button.innerText = pokemon.name;
     button.classList.add('button-class');
+    button.addEventListener('click', function (showDetails) {
+      onclick = pokemon;
+    });
 
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
-  }
-
-  //show more details of pokemon
-  function showDetails(pokemon){
-    console.log(pokemon);
   }
 
   return {
